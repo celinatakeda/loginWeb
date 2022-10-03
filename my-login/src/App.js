cc927815491261295f88155a689e6ee2bd51fbd8
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2022-10-03 11:58:32 -0300

    commit

--- my-login/src/App.js
+++ my-login/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
+import LoginPage from './pages/LoginPage';
+
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>      
+      <LoginPage />      
     </div>
   );
 }
