8c00abae36d6353ac231052377ef0e83f0f2d2d2
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2022-10-05 10:49:32 -0300

    commit

--- my-login/src/AppRoutes.jsx
+++ my-login/src/AppRoutes.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 import { 
   BrowserRouter as Router,
   Route,
@@ -8,14 +10,35 @@ import {
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
 
+import { AuthContext } from "./contexts/auth";
+
 const AppRoutes = () => {
+  const [user, setUser] = useState(null);
+
+  const login = (email, password) => {
+    console.log("login auth", { email, password });
+    setUser({ id: "123", email })
+  };
+
+  const logout = () => {
+    console.log("logout");
+  };
+
+  // user != null 
+  // authenticated = true
+   // user == null 
+  // authenticated = false
+  //!!user: tranforma num cast de boolean
+
   return (
     <Router>
-      <Routes>
-        <Route exact path="/login" element={<LoginPage />} />
-        <Route exact path="/" element={<HomePage />} />
-      </Routes>
-    </Router>
+      <AuthContext.Provider value={{authenticated:!!user, user, login, logout }}>
+        <Routes>
+          <Route exact path="/login" element={<LoginPage />} />
+          <Route exact path="/" element={<HomePage />} />
+        </Routes>
+      </AuthContext.Provider>
+    </Router>    
   );
 };
 
